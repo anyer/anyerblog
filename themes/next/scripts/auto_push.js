1b9006b12406e39c7f06559aeabb42a594a885ef
anyer <maple_6392@163.com>
2016-09-13 00:28:18 +0800

    add sitmap and robots

--- themes/next/scripts/auto_push.js
+++ themes/next/scripts/auto_push.js
@@ -20,7 +20,7 @@ function run() {
 			exit(1);
 
 		}
-		if (exec('git commit -am "Form auto backup script\'s commit"').code !== 0) {
+		if (exec('git commit -am "add sitmap and robots"').code !== 0) {
 			echo('Error: Git commit failed');
 			exit(1);
 
